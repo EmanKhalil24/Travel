fa38cea4d42193b3f8410abbad547a992749abc1
EmanKhalil24 <99218985+EmanKhalil24@users.noreply.github.com>
2022-07-18 13:55:33 +0200

    Add files via upload

---
+++ travel/travel.js
@@ -0,0 +1,48 @@
+let btn = document.getElementById("up");
+
+window.onscroll = function () {
+  if (window.scrollY >= 650) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+let lis = document.querySelectorAll("ul li");
+let exp = document.querySelector('.news');
+
+if (window.localStorage.getItem("color")) {
+  // If There Is Color In Local Storage
+  // [1] Add Color To Div
+  exp.style.backgroundColor = window.localStorage.getItem("color");
+  // [2] Remove Active Class From All Lis
+  lis.forEach((li) => {
+    li.classList.remove("active");
+  });
+  // [3] Add Active Class To Current Color
+  document.querySelector(`[data-color="${window.localStorage.getItem("color")}"]`).classList.add("active");
+}
+
+lis.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    // console.log(e.currentTarget.dataset.color);
+    // Remove Active Class From all Lis
+    lis.forEach((li) => {
+      li.classList.remove("active");
+    });
+    // Add Active Class To Current Element
+    e.currentTarget.classList.add("active");
+    // Add Current Color To Local Storage
+    window.localStorage.setItem("color", e.currentTarget.dataset.color);
+    // Change Div Background Color
+    exp.style.backgroundColor = e.currentTarget.dataset.color;
+  });
+});
